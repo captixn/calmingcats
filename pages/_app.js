3cb897a1a32f0b01a20e01e8fe08b5e084afdda4
Alexandre Pinon <captixn@gmail.com>
2021-02-16 10:42:22 +0100

    add css normalizer

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import '../styles/global.scss'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'normalize.css'
 
 export default function App({ Component, pageProps }) {
     return <Component {...pageProps} />
