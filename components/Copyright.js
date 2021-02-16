f798dc12ca53754fd6220b87ade39b0e93c8eccb
Alexandre Pinon <captixn@gmail.com>
2021-02-16 12:58:41 +0100

    changing footer to Want your cat yo become a Calming Cat

--- components/Copyright.js
+++ components/Copyright.js
@@ -3,7 +3,19 @@ import * as React from "react";
 export const Credits = React.forwardRef(({ onClick, href }, ref) => {
   return (
     <div className="credits">
-      <p><strong>Made with love by:</strong></p>
+      <p>
+        <strong>
+          Do you have photos and soothing quotes that you want to feature?
+        </strong>
+      </p>
+      <p>
+        Drop an email at <br />
+        <a href="mailto:calmingcatsme@gmail.com">calmingcatsme@gmail.com</a>
+      </p>
+      <br />
+      <p>
+        <strong>This app with made with love by:</strong>
+      </p>
       <p>
         Author :{" "}
         <a href="https://www.linkedin.com/in/acvkp/">Alexandre Pinon</a>
@@ -13,11 +25,10 @@ export const Credits = React.forwardRef(({ onClick, href }, ref) => {
         <a href="https://www.instagram.com/revsociety_/">Aurore Jacques</a>{" "}
       </p>
       <br />
-      <p><strong>With the contribution of :</strong></p>
+      <p>
+        <strong>With the contribution of :</strong>
+      </p>
       <p>Marie &amp; Léo, John Doe</p>
-      <br />
-      <p><strong>Do you have photos and soothing quotes that you want to feature?</strong></p>
-      <p>Drop an email at <br/><a href="mailto:calmingcatsme@gmail.com">calmingcatsme@gmail.com</a></p>
     </div>
   );
 });
@@ -26,9 +37,9 @@ export const Copyright = React.forwardRef(({ onClick, href }, ref) => {
   return (
     <div className="copyright">
       <p>
-        Made with love by{" "}
+        Want your cat to{" "}
         <a href="/catlovers" ref={ref} onClick={onClick}>
-          cat lovers
+          become a Calming Cat?
         </a>
       </p>
     </div>
